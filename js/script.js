fabb3b024383c4ba30138b680bfb4ecb2a6e2e4e
unknown <mauricio.borges@msn.com>
2017-11-28 05:07:59 -0200

    Projeto Final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
     $("#nav-about").click(function(){
-        $(".carousel").hide();
+        $(".header").hide();
          $(".text-center").hide();
           $(".container").hide();
            $(".about").show();
+            $(".pontos-turisticos").hide();
     });
 
     $("#nav-home").click(function(){
-        $(".carousel").show();
+        $(".header").show();
          $(".text-center").show();
           $(".container").show();
            $(".about").hide();
+            $(".pontos-turisticos").hide();
+    });
+
+    $("#dropdown-turismo").click(function(){
+        $(".header").hide();
+         $(".text-center").hide();
+          $(".container").hide();
+           $(".about").hide();
+            $(".pontos-turisticos").show();
+    });
+
+    $("#pontosTuristicos").click(function(){
+        $(".header").hide();
+         $(".text-center").hide();
+          $(".container").hide();
+           $(".about").hide();
+            $(".pontos-turisticos").show();
     });
 });
